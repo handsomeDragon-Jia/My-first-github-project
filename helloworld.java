ba2d3ed512e3c103ae176b4ddc4d92693b465ba4
handsomeDragon-Jia <58476690+handsomeDragon-Jia@users.noreply.github.com>
2019-12-03 23:17:03 +0800

    添加注释

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,8 @@
-public class helloworld{
+public class helloworld{  //创建helloworld类
 
-  public static void main(String[] args){
+  public static void main(String[] args){ //创建主方法
   
-    System.out.println("Hello World!");
+    System.out.println("Hello World!"); //输出控制台
   
   }
 
